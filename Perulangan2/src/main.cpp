3ae4a62e33a8b22216b7d7048c89979e77dd3b78
Andndre <agungandre687@gmail.com>
2023-04-21 08:36:51 +0800

    perulangan2

---
+++ Perulangan2/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hasil = 0, perulangan = 0, i = 1;
+
+    cout << i;
+
+    while (++perulangan < 10)
+    {
+        hasil += i;
+        i += 2;
+        cout << " + " << i;
+    }
+
+    hasil += i;
+    cout << " = " << hasil << endl;
+}
